3e60660ce54cb5408d149803af8a61ed690a00f9
Sheean Spoel <s.j.j.spoel@uu.nl>
2025-01-22 16:07:52 +0100

    0.1.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as file:
 setup(
     name='mwe-query',
     python_requires='>=3.8, <4',
-    version='0.0.3b1',
+    version='0.1.0',
     description='',
     long_description=long_description,
     long_description_content_type="text/markdown",
